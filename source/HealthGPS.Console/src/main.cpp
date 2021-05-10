b0ea4b81af130e76d3107a3b8ad738bf98ae329e
Israel Vieira <i.vieira@imperial.ac.uk>
2021-05-10 16:46:16 +0100

    Created initial visual studio solution for project.

---
+++ source/HealthGPS.Console/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "main.h"
+
+int main(int argc, const char* argv[])
+{
+	std::cout << "# Hello Health-GPS Microsimulation World #" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Today: " << getTimeNowStr() << std::endl;
+	std::cout << std::endl;
+
+	return 0;
+}
