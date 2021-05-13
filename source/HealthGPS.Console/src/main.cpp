82b7413b1a97004ccc5cd0ca27fa74d028b7d28e
Israel Vieira <i.vieira@imperial.ac.uk>
2021-05-13 14:54:18 +0100

    Create initial modern C++ solution using package manager.

--- source/HealthGPS.Console/src/main.cpp
+++ source/HealthGPS.Console/src/main.cpp
@@ -1,12 +1,30 @@
-#include <iostream>
 #include "main.h"
 
-int main(int argc, const char* argv[])
+int main(int argc, char* argv[])
 {
-	std::cout << "# Hello Health-GPS Microsimulation World #" << std::endl;
-	std::cout << std::endl;
-	std::cout << "Today: " << getTimeNowStr() << std::endl;
-	std::cout << std::endl;
+	fmt::print(fg(fmt::color::yellow) | bg(fmt::color::blue) |
+		fmt::emphasis::bold, "\n# Hello Health-GPS Microsimulation World #\n\n");
 
-	return 0;
+	fmt::print("Today: {}\n\n", getTimeNowStr());
+
+	auto options = create_options();
+
+	auto result = options.parse(argc, argv);
+	if (result.count("help"))
+	{
+		std::cout << options.help() << std::endl;
+		return EXIT_SUCCESS;
+	}
+
+	if (result.count("file"))
+	{
+		auto file_name = result["file"].as<std::string>();
+		auto config = create_json();
+		fmt::print("Configuration file: {}\n{}\n", file_name, config.dump(4));
+	}
+
+	// TDD macro-free framework
+	boost::ut::expect(true);
+
+	return EXIT_SUCCESS;
 }
