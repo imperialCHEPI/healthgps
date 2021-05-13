a1728a6e76614e2f16d8943d507114fe471fd407
Israel Vieira <78531031+israel-vieira@users.noreply.github.com>
2021-05-13 16:33:28 +0100

    Added modern tools chain (#9)
    Create initial modern C++ solution using package manager and test frameworks

---
+++ source/HealthGPS.Console/include/main.h
@@ -0,0 +1,61 @@
+#ifndef HEALTHGPS_CONSOLE_HPP_INCLUDED
+#define HEALTHGPS_CONSOLE_HPP_INCLUDED
+
+#include <chrono>
+#include <ctime>
+#include <iostream>
+#include <fmt/core.h>
+#include <fmt/color.h>
+#include <cxxopts.hpp>
+#include <nlohmann/json.hpp>
+
+namespace hgps
+{
+	using json = nlohmann::json;
+
+	std::string getTimeNowStr() {
+		std::time_t now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+		std::string s(30, '\0');
+
+		struct tm localtime;
+
+		localtime_s(&localtime, &now);
+
+		std::strftime(&s[0], s.size(), "%c", &localtime);
+
+		return s;
+	}
+
+	cxxopts::Options create_options()
+	{
+		cxxopts::Options options("C++20 Study", "Creates a modern C++ tool chain study.");
+		options.add_options()
+			("h,help", "Help about this application.")
+			("f,file", "File name", cxxopts::value<std::string>())
+			("v,verbose", "Verbose output", cxxopts::value<bool>()->default_value("false"));
+
+		return options;
+	}
+
+	json create_json()
+	{
+		json j2 = {
+			{"pi", 3.141},
+			{"happy", true},
+			{"name", "Boris"},
+			{"nothing", nullptr},
+			{"answer", {
+				{"everything", 13}
+			}},
+			{"list", {1, 3, 7, 0}},
+			{"object", {
+				{"currency", "GBP"},
+				{"value", 75.13}
+			}}
+		};
+
+		return j2;
+	}
+}
+
+#endif //HEALTHGPS_CONSOLE_HPP_INCLUDED
--- source/HealthGPS.Console/src/main.cpp
+++ source/HealthGPS.Console/src/main.cpp
@@ -1,12 +1,33 @@
-#include <iostream>
+#include "boost/ut.hpp"
 #include "main.h"
 
-int main(int argc, const char* argv[])
-{
-	std::cout << "# Hello Health-GPS Microsimulation World #" << std::endl;
-	std::cout << std::endl;
-	std::cout << "Today: " << getTimeNowStr() << std::endl;
-	std::cout << std::endl;
+using namespace hgps;
 
-	return 0;
+int main(int argc, char* argv[])
+{
+	fmt::print(fg(fmt::color::yellow) | bg(fmt::color::blue) |
+		fmt::emphasis::bold, "\n# Hello Health-GPS Microsimulation World #\n\n");
+
+	fmt::print("Today: {}\n\n", getTimeNowStr());
+
+	auto options = create_options();
+
+	auto result = options.parse(argc, argv);
+	if (result.count("help"))
+	{
+		std::cout << options.help() << std::endl;
+		return EXIT_SUCCESS;
+	}
+
+	if (result.count("file"))
+	{
+		auto file_name = result["file"].as<std::string>();
+		auto config = create_json();
+		fmt::print("Configuration file: {}\n{}\n", file_name, config.dump(4));
+	}
+
+	// TDD macro-free framework
+	boost::ut::expect(true);
+
+	return EXIT_SUCCESS;
 }
