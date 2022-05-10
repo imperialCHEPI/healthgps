2628f1f948716c5bf19b60411f95f31694fcbe8c
Israel Vieira <i.vieira@imperial.ac.uk>
2022-05-10 10:44:45 +0100

    Update example model with latest data and more fitting.

--- src/HealthGPS.Console/program.cpp
+++ src/HealthGPS.Console/program.cpp
@@ -124,7 +124,7 @@ int main(int argc, char* argv[])
 				SimulationDefinition{ model_input, std::move(policy_scenario), std::move(policy_rnd) },
 				factory, event_bus };
 
-			fmt::print(fg(fmt::color::cyan), "\nStarting intervention simulation ...\n\n");
+			fmt::print(fg(fmt::color::cyan), "\nStarting intervention simulation with {} trials ...\n\n", config.trial_runs);
 			auto worker = std::jthread{ [&runtime, &executive, &baseline, &intervention, &config, &done] {
 				runtime = executive.run(baseline, intervention, config.trial_runs);
 				done.store(true);
@@ -137,7 +137,7 @@ int main(int argc, char* argv[])
 			worker.join();
 		}
 		else {
-			fmt::print(fg(fmt::color::cyan), "\nStarting baseline simulation ...\n\n");
+			fmt::print(fg(fmt::color::cyan), "\nStarting baseline simulation with {} trials ...\n\n", config.trial_runs);
 			channel.close(); // Will not store any message
 			auto worker = std::jthread{ [&runtime, &executive, &baseline, &config, &done] {
 				runtime = executive.run(baseline, config.trial_runs);
