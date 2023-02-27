8032a8eef6b6342ac2d3fe57fe03debdf122e6ae
Israel Vieira <i.vieira@imperial.ac.uk>
2023-02-27 11:06:15 +0000

    Added simulation executive and engine documentation.

--- src/HealthGPS/modelrunner.h
+++ src/HealthGPS/modelrunner.h
@@ -9,18 +9,48 @@
 
 namespace hgps {
 
+	/// @brief Defines the simulation executive data type class.
+	///
+	/// @details The simulation executive holds the simulation engine instances
+	/// and manages the execution of experiments by completing multiple runs
+	/// of the same scenario. The executive controls the number of runs required,
+	/// current run number, progress notifications, random number generator seeds
+	/// for each run, and the parallel evaluation for the two scenarios..
+	/// 
+	/// Health-GPS uses a simpler version of adevs (A Discrete EVent System simulator)
+	/// library (https://sourceforge.net/projects/bdevs), which contain only four header
+	/// files, but provides an intuitive modelling interface for agent-based models,
+	/// without requiring familiarity with the full aspects of the DEVS formalism.
 	class ModelRunner
 	{
 	public:
 		ModelRunner() = delete;
+
+		/// @brief Initialises a new instance of the ModelRunner class.
+		/// @param bus The message bus instance to use for notification
+		/// @param generator Random number generator for runs management
 		ModelRunner(EventAggregator& bus, std::unique_ptr<RandomBitGenerator> generator) noexcept;
 
+		/// @brief Run an experiment for baseline scenario only
+		/// @param baseline The simulation engine instance
+		/// @param trial_runs Experiment number of runs
+		/// @return The experiment total elapsed time, in milliseconds
+		/// @throws std::invalid_argument for non-baseline scenario type or negative number of runs.
 		double run(Simulation& baseline, const unsigned int trial_runs);
 
+		/// @brief Run an experiment for baseline and intervention scenarios
+		/// @param baseline The simulation engine instance for baseline scenario
+		/// @param intervention The simulation engine instance for intervention scenario
+		/// @param trial_runs Experiment number of runs
+		/// @return The experiment total elapsed time, in milliseconds
+		/// @throws std::invalid_argument for scenario type mismatch or negative number of runs.
 		double run(Simulation& baseline, Simulation& intervention, const unsigned int trial_runs);
 		
+		/// @brief Gets a value indicating whether an experiment is current running.
+		/// @return true, if a experiment is underway; otherwise, false.
 		bool is_running() const noexcept;
 
+		/// @brief Cancel a running experiment.
 		void cancel() noexcept;
 
 	private:
