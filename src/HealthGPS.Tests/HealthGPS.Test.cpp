0843562aabeeb691bb4264e11675c3379c6cb56d
Alex Dewar <a.dewar@imperial.ac.uk>
2023-09-19 11:42:20 +0100

    Remove a couple of static functions

--- src/HealthGPS.Tests/HealthGPS.Test.cpp
+++ src/HealthGPS.Tests/HealthGPS.Test.cpp
@@ -13,9 +13,10 @@
 #include <map>
 #include <optional>
 
-static std::vector<hgps::MappingEntry> create_mapping_entries() {
-    return {{"Gender", 0}, {"Age", 0}, {"SmokingStatus", 1}, {"AlcoholConsumption", 1}, {"BMI", 2}};
-}
+namespace {
+std::vector<hgps::MappingEntry> mapping_entries{
+    {{"Gender", 0}, {"Age", 0}, {"SmokingStatus", 1}, {"AlcoholConsumption", 1}, {"BMI", 2}}};
+} // anonymous namespace
 
 void create_test_datatable(hgps::core::DataTable &data) {
     using namespace hgps;
@@ -61,8 +62,7 @@ hgps::ModelInput create_test_configuration(hgps::core::DataTable &data) {
         {"gender", "Gender"}, {"age", "Age"}, {"education", "Education"}, {"income", "Income"}};
     auto ses = SESDefinition{.fuction_name = "normal", .parameters = std::vector<double>{0.0, 1.0}};
 
-    auto entries = create_mapping_entries();
-    auto mapping = HierarchicalMapping(std::move(entries));
+    auto mapping = HierarchicalMapping(mapping_entries);
 
     auto diseases = std::vector<core::DiseaseInfo>{
         DiseaseInfo{
