873ac9f97ff92aa390777f91f0b31d20672225d0
James Paul Turner <james.turner@imperial.ac.uk>
2023-09-22 13:08:32 +0100

    Add a struct containing linear models for food groups.

--- src/HealthGPS/energy_balance_model.h
+++ src/HealthGPS/energy_balance_model.h
@@ -8,6 +8,12 @@
 
 namespace hgps {
 
+/// @brief Defines the linear models used to initialise a Person's food values
+struct FoodLinearModels {
+    std::unordered_map<core::Identifier, double> intercept;
+    std::unordered_map<core::Identifier, std::unordered_map<core::Identifier, double>> coefficients;
+};
+
 /// @brief State data type for a simulated person
 struct SimulatePersonState {
     /// @brief Height
