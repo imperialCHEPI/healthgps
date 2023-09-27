c2918a78ef942071c1ce8f38e6e1a0b1da019387
Alex Dewar <a.dewar@imperial.ac.uk>
2023-09-27 11:22:16 +0100

    Add code for loading mapping between food and macronutrients

---
+++ src/HealthGPS.Tests/program_path.cpp
@@ -0,0 +1,44 @@
+#include "program_path.h"
+
+#ifdef __linux__
+#include <limits.h>
+#include <unistd.h>
+#endif
+#ifdef _WIN32
+#include <windows.h>
+#endif
+#ifdef __APPLE__
+#include <mach-o/dyld.h>
+#endif
+
+#include "HealthGPS.Core/exception.h"
+
+std::filesystem::path get_program_directory() { return get_program_path().parent_path(); }
+
+std::filesystem::path get_program_path() {
+    bool success;
+
+#if defined(__linux__)
+    char path[PATH_MAX + 1];
+    ssize_t len = readlink("/proc/self/exe", path, PATH_MAX);
+    if (!(success = (len >= 0))) {
+        path[len] = '\0';
+    }
+#elif defined(_WIN32)
+    wchar_t path[MAX_PATH];
+    DWORD len = GetModuleFileNameW(nullptr, path, MAX_PATH);
+    success = (len > 0);
+#elif defined(__APPLE__)
+    char path[MAXPATHLEN + 1];
+    uint32_t len = sizeof(path);
+    success = (_NSGetExecutablePath(path, &len) == 0);
+#else
+#error "Unsupported platform"
+#endif
+
+    if (!success) {
+        throw hgps::core::HgpsException("Could not get program path");
+    }
+
+    return std::filesystem::path{path};
+}
---
+++ src/HealthGPS/nutrients.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "HealthGPS.Core/identifier.h"
+
+#include "rapidcsv.h"
+
+#include <unordered_map>
+#include <vector>
+
+namespace hgps {
+namespace detail {
+/// @brief Work out which column each macronutrient isin
+/// @param column_names Column names for CSV file
+/// @param nutrients Macronutrients
+/// @return Index for each macronutrient in order
+std::vector<size_t> get_nutrient_indexes(const std::vector<std::string> &column_names,
+                                         const std::vector<core::Identifier> &nutrients);
+
+std::unordered_map<core::Identifier, std::vector<double>>
+get_nutrient_table(rapidcsv::Document &doc, const std::vector<core::Identifier> &nutrients,
+                   const std::vector<size_t> &nutrient_idx);
+} // namespace detail
+
+/// @brief Load nutrient information from a CSV file
+/// @param csv_path Path to CSV file
+/// @param nutrients Names of macronutrients
+/// @return Mapping from food name to constituent macronutrients
+std::unordered_map<core::Identifier, std::vector<double>>
+load_nutrient_table(const std::string &csv_path, const std::vector<core::Identifier> &nutrients);
+} // namespace hgps
