c404c0a3413cb367cc249a05995d14929d1d216b
James Paul Turner <jamesturner246@users.noreply.github.com>
2023-10-20 19:10:53 +0100

    Don't std move trivial type
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/HealthGPS.Console/model_parser.cpp
+++ src/HealthGPS.Console/model_parser.cpp
@@ -295,7 +295,7 @@ load_kevinhall_risk_model_definition(const poco::json &opt, const host::Configur
         rural_prevalence;
     for (const auto &age_group : opt["RuralPrevalence"]) {
         auto age_group_range = age_group["AgeRange"].get<hgps::core::IntegerInterval>();
-        rural_prevalence[std::move(age_group_range)] = {
+        rural_prevalence[age_group_range] = {
             {hgps::core::Gender::female, age_group["Female"]},
             {hgps::core::Gender::male, age_group["Male"]}};
     }
