306ef2fc7e21d7f4c8df467f8beb0daa52f8981f
TinyMarsh <ranismyth@gmail.com>
2024-02-22 15:23:46 +0000

    PR suggestions

--- src/HealthGPS.Tests/StaticLinearModel.Test.cpp
+++ src/HealthGPS.Tests/StaticLinearModel.Test.cpp
@@ -18,21 +18,21 @@ class ModelParameters : public::testing::Test {
 
         void SetUp() override {
             auto expected = hgps::RiskFactorSexAgeTable{};
-            std::vector<double> factorsMale = {200.,40.,30.,2000.,1.,1000.,3.,50.};
-            std::vector<double> factorsFemale = {200.,40.,30.,2000.,1.,1000.,3.,50.};
+            std::vector<double> factor_means_male = {200.,40.,30.,2000.,1.,1000.,3.,50.};
+            std::vector<double> factor_means_female = {200.,40.,30.,2000.,1.,1000.,3.,50.};
 
-            std::unordered_map<hgps::core::Identifier, std::vector<double>> factorsMale = {
-                {hgps::core::Identifier{"0"}, factorsMale},
-                {hgps::core::Identifier{"1"}, factorsMale}
+            std::unordered_map<hgps::core::Identifier, std::vector<double>> factors_male = {
+                {hgps::core::Identifier{"FoodCarbohydrate"}, factor_means_male},
+                {hgps::core::Identifier{"FoodFat"}, factor_means_male}
             };
 
-            std::unordered_map<hgps::core::Identifier, std::vector<double>> factorsFemale = {
-                {hgps::core::Identifier{"0"}, factorsFemale},
-                {hgps::core::Identifier{"1"}, factorsFemale}
+            std::unordered_map<hgps::core::Identifier, std::vector<double>> factors_female = {
+                {hgps::core::Identifier{"FoodCarbohydrate"}, factor_means_female},
+                {hgps::core::Identifier{"FoodFat"}, factor_means_female}
             };
 
-            expected.emplace_row(hgps::core::Gender::male, factorsMale);
-            expected.emplace_row(hgps::core::Gender::female, factorsFemale);
+            expected.emplace_row(hgps::core::Gender::male, factors_male);
+            expected.emplace_row(hgps::core::Gender::female, factors_female);
 
             std::vector<hgps::core::Identifier> names = {"FoodCarbohydrate", "FoodFat"};
 
@@ -40,8 +40,8 @@ class ModelParameters : public::testing::Test {
             // We will define 2 RiskFactorModels: carb_model and fat_model 
             std::vector<hgps::LinearModelParams> models;
             hgps::LinearModelParams carb_model;
-            carb_model.intercept = -0.241505734056409;
-            std::unordered_map<hgps::core::Identifier, double> coefficients = {
+            carb_model.intercept = -0.241505734056409; 
+            carb_model.coefficients = {
                 {"Gender", 0.00191412437431455},
                 {"Age", -0.000545257990453302},
                 {"Age2", 3.06434596341924e-6},
@@ -49,11 +49,10 @@ class ModelParameters : public::testing::Test {
                 {"Sector", 0.0967308121487847},
                 {"Income", 0.0810023788842083}
             };
-            carb_model.coefficients = coefficients;
 
             hgps::LinearModelParams fat_model;
             fat_model.intercept = -0.693521187677;
-            coefficients = {
+            fat_model.coefficients = {
                 {"Gender", 0.00385126434589752},
                 {"Age", -0.00433711587715954},
                 {"Age2", 3.11799231385975e-5},
@@ -61,7 +60,6 @@ class ModelParameters : public::testing::Test {
                 {"Sector", -0.0629531974852436},
                 {"Income", 0.373256996730791}
             };
-            fat_model.coefficients = coefficients;
 
             models.emplace_back(std::move(carb_model));
             models.emplace_back(std::move(fat_model));
@@ -84,38 +82,34 @@ class ModelParameters : public::testing::Test {
             // We need 2 policy models for the 2 risk factor models
             hgps::LinearModelParams carb_policy_model;
             carb_policy_model.intercept = -1.29385215316281;
-            coefficients = {
+            carb_policy_model.coefficients = {
                 {"Gender", 0.00845121355575458},
                 {"Age", 0.000386003599279214},
                 {"Sector", 0.0621763530502617},
                 {"Income", 0.0560558815729017}
             };
-            carb_policy_model.coefficients = coefficients;
-            std::unordered_map<hgps::core::Identifier, double> log_coefficients = {
+            carb_policy_model.log_coefficients = {
                 {"FoodCarbohydrate", 0.577638667323293},
                 {"FoodFat", -0.0801300827856402},
                 {"FoodProtein", -0.480946893299471},
                 {"FoodSodium", -0.0396319735508116}
             };
-            carb_policy_model.log_coefficients = log_coefficients;
             policy_ranges.emplace_back(hgps::core::DoubleInterval{-2.31063, 0.37526});
 
             hgps::LinearModelParams fat_policy_model;
             fat_policy_model.intercept = -0.734121205356728;
-            coefficients = {
+            fat_policy_model.coefficients = {
                 {"Gender", 0.023422206650121},
                 {"Age", 0.000557687923688474},
                 {"Sector", 0.00166651997223223},
                 {"Income", -0.498755023022772}
-            }
-            fat_policy_model.coefficients = coefficients;
-            log_coefficients = {
+            };
+            fat_policy_model.log_coefficients = {
                 {"FoodCarbohydrate", 0.652615267035516},
                 {"FoodFat", 0.743457905132894},
                 {"FoodProtein", -1.2648185364167},
                 {"FoodSodium", -0.17181077457271}
             };
-            fat_policy_model.log_coefficients = log_coefficients;
             policy_ranges.emplace_back(hgps::core::DoubleInterval{-4.87205, -0.10346});
 
             policy_models.emplace_back(std::move(carb_policy_model));
@@ -131,26 +125,23 @@ class ModelParameters : public::testing::Test {
             
             hgps::LinearModelParams income_model_low;
             income_model_low.intercept = 0.0;
-            coefficients = {}
-            income_model_low.coefficients = coefficients;
+            income_model_low.coefficients = {};
 
             hgps::LinearModelParams income_model_middle;
             income_model_middle.intercept = -0.0023671151435328;
-            coefficients = {
+            income_model_middle.coefficients = {
                 {"Gender", 0.0244784583947992},
                 {"Over18", 0.276669591410528},
                 {"Sector", -0.491794449118805}
             };
-            income_model_middle.coefficients = coefficients;
 
             hgps::LinearModelParams income_model_high;
             income_model_high.intercept = 0.0187952471153142;
-            coefficients = {
+            income_model_high.coefficients = {
                 {"Gender", 0.02018185354194},
                 {"Over18", 0.607099252829797},
                 {"Sector", -1.5870837069653}
             };
-            income_model_high.coefficients = coefficients;
 
             income_models.emplace(hgps::core::Income::low, std::move(income_model_low));
             income_models.emplace(hgps::core::Income::middle, std::move(income_model_middle));
