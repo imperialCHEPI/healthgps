6243469ef8e5ce374fb4a9a12a1d321048392d11
James Paul Turner <james.turner@imperial.ac.uk>
2024-05-02 12:52:25 +0100

    Load nutrient ranges into static linear model.

--- src/HealthGPS/static_linear_model.h
+++ src/HealthGPS/static_linear_model.h
@@ -26,6 +26,7 @@ class StaticLinearModel final : public RiskFactorAdjustableModel {
     /// @param expected The risk factor expected values by sex and age
     /// @param names The risk factor names
     /// @param models The linear models used to compute a person's risk factor values
+    /// @param ranges The risk factor value boundaries
     /// @param lambda The lambda values of the risk factors
     /// @param stddev The standard deviations of the risk factors
     /// @param cholesky Cholesky decomposition of the risk factor correlation matrix
@@ -39,7 +40,8 @@ class StaticLinearModel final : public RiskFactorAdjustableModel {
     /// @throws HgpsException for invalid arguments
     StaticLinearModel(
         const RiskFactorSexAgeTable &expected, const std::vector<core::Identifier> &names,
-        const std::vector<LinearModelParams> &models, const std::vector<double> &lambda,
+        const std::vector<LinearModelParams> &models,
+        const std::vector<core::DoubleInterval> &ranges, const std::vector<double> &lambda,
         const std::vector<double> &stddev, const Eigen::MatrixXd &cholesky,
         const std::vector<LinearModelParams> &policy_models,
         const std::vector<core::DoubleInterval> &policy_ranges,
@@ -100,6 +102,7 @@ class StaticLinearModel final : public RiskFactorAdjustableModel {
 
     const std::vector<core::Identifier> &names_;
     const std::vector<LinearModelParams> &models_;
+    const std::vector<core::DoubleInterval> &ranges_;
     const std::vector<double> &lambda_;
     const std::vector<double> &stddev_;
     const Eigen::MatrixXd &cholesky_;
