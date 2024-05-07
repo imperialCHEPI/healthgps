39f3849a45b85f87905316c4a6505be610e59a91
James Paul Turner <james.turner@imperial.ac.uk>
2024-05-07 16:59:55 +0100

    Comments: range not boundary.

--- src/HealthGPS/static_linear_model.h
+++ src/HealthGPS/static_linear_model.h
@@ -26,12 +26,12 @@ class StaticLinearModel final : public RiskFactorAdjustableModel {
     /// @param expected The risk factor expected values by sex and age
     /// @param names The risk factor names
     /// @param models The linear models used to compute a person's risk factor values
-    /// @param ranges The risk factor value boundaries
+    /// @param ranges The value range of each risk factor
     /// @param lambda The lambda values of the risk factors
     /// @param stddev The standard deviations of the risk factors
     /// @param cholesky Cholesky decomposition of the risk factor correlation matrix
     /// @param policy_models The linear models used to compute a person's intervention policies
-    /// @param policy_ranges The intervention policy value boundaries
+    /// @param policy_ranges The value range of each intervention policy
     /// @param policy_cholesky Cholesky decomposition of the intervention policy covariance matrix
     /// @param info_speed The information speed of risk factor updates
     /// @param rural_prevalence Rural sector prevalence for age groups and sex
@@ -125,12 +125,12 @@ class StaticLinearModelDefinition : public RiskFactorAdjustableModelDefinition {
     /// @param expected The risk factor expected values by sex and age
     /// @param names The risk factor names
     /// @param models The linear models used to compute a person's risk factors
-    /// @param ranges The risk factor value boundaries
+    /// @param ranges The value range of each risk factor
     /// @param lambda The lambda values of the risk factors
     /// @param stddev The standard deviations of the risk factors
     /// @param cholesky Cholesky decomposition of the risk factor correlation matrix
     /// @param policy_models The linear models used to compute a person's intervention policies
-    /// @param policy_ranges The intervention policy value boundaries
+    /// @param policy_ranges The value range of each intervention policy
     /// @param policy_cholesky Cholesky decomposition of the intervention policy covariance matrix
     /// @param info_speed The information speed of risk factor updates
     /// @param rural_prevalence Rural sector prevalence for age groups and sex
