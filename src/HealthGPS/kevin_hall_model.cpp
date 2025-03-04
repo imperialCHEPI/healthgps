287af599190333d5057cd566ce9993a09bbd1d91
Mahima Ghosh <mg423@ic.ac.uk>
2025-03-04 19:35:46 +0000

    Updated tests and model implementation for improved coverage

--- src/HealthGPS/kevin_hall_model.cpp
+++ src/HealthGPS/kevin_hall_model.cpp
@@ -1,5 +1,21 @@
 #include "HealthGPS.Core/exception.h"
 
+// Disable specific warnings from external dependencies
+#ifdef _MSC_VER
+#pragma warning(push)
+// Eigen warnings
+#pragma warning(disable: 26495) // Variable 'xxx' is uninitialized (from Eigen SIMD optimizations)
+
+// fmt library warnings
+#pragma warning(disable: 26498) // Variable 'xxx' is constexpr, mark variable constexpr (from fmt)
+
+// lexer warnings
+#pragma warning(disable: 26819) // Unannotated fallthrough between switch labels (from lexer)
+
+// Low-level bitwise operations
+#pragma warning(disable: 6285)  // Non-zero constant in bitwise-and operation (from core bit manipulation)
+#endif
+
 #include "kevin_hall_model.h"
 #include "runtime_context.h"
 #include "sync_message.h"
@@ -8,6 +24,10 @@
 #include <iterator>
 #include <random>
 #include <utility>
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
 
 namespace { // anonymous namespace
 
@@ -48,7 +68,8 @@ KevinHallModel::KevinHallModel(
       height_stddev_{height_stddev}, height_slope_{height_slope},
       region_models_{std::move(region_models)}, ethnicity_models_{std::move(ethnicity_models)},
       income_models_{std::move(income_models)},
-      income_continuous_stddev_{income_continuous_stddev} {}
+      income_continuous_stddev_{income_continuous_stddev},
+      physical_activity_stddev_{0.5} {}
 
 RiskFactorModelType KevinHallModel::type() const noexcept { return RiskFactorModelType::Dynamic; }
 
