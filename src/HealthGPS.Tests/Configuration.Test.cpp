063b551cdc0eb6901ace289a632c32d0b00002f7
Mahima Ghosh <mg423@ic.ac.uk>
2026-02-19 17:58:40 +0000

    fixed individul ID tests

--- src/HealthGPS.Tests/Configuration.Test.cpp
+++ src/HealthGPS.Tests/Configuration.Test.cpp
@@ -634,7 +634,7 @@ TEST_F(ConfigParsingFixture, LoadRunningInfo) {
 
 TEST_F(ConfigParsingFixture, LoadOutputInfo) {
     const OutputInfo output_info{
-        .comorbidities = 3, .folder = "/home/test", .file_name = "filename.txt"};
+        .comorbidities = 3, .folder = "/home/test", .file_name = "filename.txt", .individual_id_tracking = std::nullopt};
     const auto valid_output_info = [&]() {
         json j;
         j["output"] = output_info;
